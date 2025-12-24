da6ff5cb34ca771699ef49836fbdc6644fecf44c
Anastasiya Luhavets <lolnysha@yandex.ru>
2025-12-24 23:27:48 +0300

    new_commit

--- tests.py
+++ tests.py
@@ -15,14 +15,6 @@ class TestBooksCollector:
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
